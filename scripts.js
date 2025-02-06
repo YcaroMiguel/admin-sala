11ea971d4ab690f8272053141b58de28cec47882
YcaroMiguel <144180288+YcaroMiguel@users.noreply.github.com>
2025-02-05 19:09:32 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
 window.onload = function() {
     alert("Bem-vindo ao site de Administração da Sala!");
 }
-const tarefas = [
-    { nome: "Matemática: Resolver página 10", prazo: "Sexta-feira" },
-    { nome: "História: Estudo sobre a Revolução Francesa", prazo: "Segunda-feira" }
-];
-
-const tarefasSection = document.getElementById('tarefas');
-let listaTarefas = "<ul>";
-
-tarefas.forEach(tarefa => {
-    listaTarefas += `<li><strong>${tarefa.nome}:</strong> Entregar até ${tarefa.prazo}.</li>`;
-});
-
-listaTarefas += "</ul>";
-tarefasSection.innerHTML = listaTarefas;
